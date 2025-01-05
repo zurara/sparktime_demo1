21ed64dd8a1b6fa5dd68dae18efc24310b7a66de
zurara <charlotte.zurara@gmail.com>
2025-01-05 16:18:26 +0800

    Center

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import ChatInterface from "@/components/chat-interface"
 
 export default function Page() {
-  return <ChatInterface />
+  return (
+    <div className="min-h-screen flex items-center justify-center p-4">
+      <ChatInterface />
+    </div>
+  )
 }
